7b659e1b688905bd13b508af7a918d8c0f10c362
RenatoSRibeiro <renato.development.ribeiro@gmail.com>
2025-03-14 19:24:12 -0300

    Arquivos Sorteador de números

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    console.log(`Quantidade de números: ${quantidade}`);
+    console.log(`Do número: ${de}`);
+    console.log(`Até o número: ${ate}`);
+}
