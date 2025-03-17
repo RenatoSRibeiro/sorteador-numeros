b48b7d64b784f9a5c4be3d5333721076385ea9cb
RenatoSRibeiro <renato.development.ribeiro@gmail.com>
2025-03-17 20:26:33 -0300

    Inclusão da função obterNumeroAleatorio

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
+    let sorteados = [];
+
+    for (let i = 0; i < quantidade; i++) {
+        let numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+    
     console.log(`Quantidade de números: ${quantidade}`);
     console.log(`Do número: ${de}`);
     console.log(`Até o número: ${ate}`);
+    console.log(`Números Sorteado: ${sorteados}`);
 }
+
+function obterNumeroAleatorio(min, max) {
+    return parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+
+
+// criar uma váriavel chamada sorteados e incluir o array dentro da função sortear
+//Criar um loop para que seja verificado a quantidade de números que serão sorteados.
+//Dentro do loop atribuir a função de obter números aleatórios e passar o parametro
+//  esperado e incluir os números no array usando o push.
