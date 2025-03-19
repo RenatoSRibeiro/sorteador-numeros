562731694775d34184c80f8eb4fc1d4d480aa2ed
RenatoSRibeiro <renato.development.ribeiro@gmail.com>
2025-03-19 11:44:02 -0300

    Verifica lista de números repetidos

--- app.js
+++ app.js
@@ -4,25 +4,32 @@ function sortear() {
     let ate = parseInt(document.getElementById('ate').value);
 
     let sorteados = [];
+    let numero;
 
     for (let i = 0; i < quantidade; i++) {
-        let numero = obterNumeroAleatorio(de, ate);
+        numero = obterNumeroAleatorio(de, ate);
+     while (sorteados.includes(numero)){
+        numero = obterNumeroAleatorio(de, ate);
+     }
         sorteados.push(numero);
     }
-    
+
     console.log(`Quantidade de números: ${quantidade}`);
     console.log(`Do número: ${de}`);
     console.log(`Até o número: ${ate}`);
     console.log(`Números Sorteado: ${sorteados}`);
 
-    exibirTextoNaTela('#resultado', sorteados);
+    //exibirTextoNaTela('#resultado', sorteados);
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = (`<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`);
 }
 
 function obterNumeroAleatorio(min, max) {
     return parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
-function exibirTextoNaTela (IdLabel, IdResultado){
+/*function exibirTextoNaTela (IdLabel, IdResultado){
     let campo = document.querySelector(IdLabel);
     campo.innerHTML = IdResultado;
-}
+}*/
