43e4c0806dd3dc22bf1a173b93f4ac44b6520838
RenatoSRibeiro <renato.development.ribeiro@gmail.com>
2025-03-20 18:21:23 -0300

    Funcionalidades botão Reiniciar

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ function sortear() {
 
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
-     while (sorteados.includes(numero)){
-        numero = obterNumeroAleatorio(de, ate);
-     }
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
         sorteados.push(numero);
     }
 
@@ -19,17 +20,38 @@ function sortear() {
     console.log(`Até o número: ${ate}`);
     console.log(`Números Sorteado: ${sorteados}`);
 
-    //exibirTextoNaTela('#resultado', sorteados);
-
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = (`<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`);
+
+    alterarStatusBotao();
+
 }
 
 function obterNumeroAleatorio(min, max) {
     return parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
-/*function exibirTextoNaTela (IdLabel, IdResultado){
-    let campo = document.querySelector(IdLabel);
-    campo.innerHTML = IdResultado;
-}*/
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').value = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
+
+
+
+
+
+
